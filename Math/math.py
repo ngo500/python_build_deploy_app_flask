ec4709a469114a601b6c0f719b389a810ec0dc0a
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-03-01 11:18:27 -0800

    Create math.py

---
+++ Math/math.py
@@ -0,0 +1,20 @@
+def summation(x, y):
+    """
+    Take x and y, and return their sum
+    """
+    result = x + y
+    return result
+
+def subtraction(x, y):
+    """
+    Take x and y, and return their difference
+    """
+    result = x - y
+    return result
+
+def multiplcation(x, y):
+    """
+    Take x and y, and return their product
+    """
+    result = x * y
+    return result
