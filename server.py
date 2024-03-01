e2a1d09ef3293d2c10f3b1c75e325cd218820743
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-03-01 11:27:16 -0800

    Update server.py, added function logic

--- server.py
+++ server.py
@@ -1,29 +1,52 @@
 from flask import Flask, render_template, request
-# Import the Maths package here
+from Math.math import summation, subtraction, multiplcation
 
 app = Flask("Mathematics Problem Solver")
 
-@app.route("/sum")
+@app.route("/sum", methods=['GET'])
 def sum_route():
+    """
+    Get 2 float arguments from request, then return their sum as a string
+    Returns:
+    String result of sum of 2 given floats
+    """
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    result = summation(num1, num2)
+    return str(result)
 
-@app.route("/sub")
+@app.route("/sub", methods=['GET'])
 def sub_route():
+    """
+    Get 2 float arguments from request, then return their difference as a string
+    Returns:
+    String result of difference of 2 given floats
+    """
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    result = subtraction(num1, num2)
+    return str(result)
 
-@app.route("/mul")
+@app.route("/mul", methods=['GET'])
 def mul_route():
+    """
+    Get 2 float arguments from request, then return their product as a string
+    Returns:
+    String result of product of 2 given floats
+    """
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here  
+    result = multiplcation(num1, num2)
+    return str(result)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def render_index_page():
-    # Write your code here
-    
+    """
+    Render the index.html page
+    Return:
+    Always renders the index.html page
+    """
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
